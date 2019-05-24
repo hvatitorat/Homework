44edfe1b6882ba63fd0b65f0f296163023d3cb96
hvatitorat <49493982+hvatitorat@users.noreply.github.com>
2019-05-25 02:45:40 +0300

    second commit

--- QuickSort.java
+++ QuickSort.java
@@ -20,7 +20,7 @@ public class QuickSort {
         System.out.println(Arrays.toString(array));
     }
 
-    private static void swap(int[] array, int ind1, int ind2) {
+    public static void swap(int[] array, int ind1, int ind2) {
         int tmp = array[ind1];
         array[ind1] = array[ind2];
         array[ind2] = tmp;
